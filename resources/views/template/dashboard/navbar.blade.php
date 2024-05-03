550a434044d9bd31f9f3f7dce3cc9260f051e5df
bla <bla@mail.com>
2024-05-03 19:33:12 +0700

    update navbar and sidebar dashboard

--- resources/views/template/dashboard/navbar.blade.php
+++ resources/views/template/dashboard/navbar.blade.php
@@ -1,19 +1,21 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-    <a class="navbar-brand brand-logo" href="{{ route('dashboard') }}"><img src="../assets/images/logo.svg" alt="logo" /></a>
-    <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}"><img src="../assets/images/logo-mini.svg"
-        alt="logo" /></a>
+    <a class="navbar-brand brand-logo" href="{{ route('dashboard') }}"><img
+        src="{{ asset('assets/images/logo.svg') }}" alt="logo" /></a>
+    <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}"><img
+        src="{{ asset('assets/images/logo-mini.svg') }}" alt="logo" /></a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-stretch">
-    <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+    <button class="navbar-toggler navbar-toggler align-self-center" type="button"
+      data-toggle="minimize">
       <span class="mdi mdi-menu"></span>
     </button>
     <ul class="navbar-nav navbar-nav-right">
       <li class="nav-item nav-profile dropdown">
-        <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-bs-toggle="dropdown"
-          aria-expanded="false">
+        <a class="nav-link dropdown-toggle" id="profileDropdown" href="#"
+          data-bs-toggle="dropdown" aria-expanded="false">
           <div class="nav-profile-img">
-            <img src="../assets/images/faces/face1.jpg" alt="image">
+            <img src="{{ asset('assets/images/faces/face1.jpg') }}" alt="image">
           </div>
           <div class="nav-profile-text">
             <p class="mb-1 text-black">{{ Auth::user()->name }}</p>
